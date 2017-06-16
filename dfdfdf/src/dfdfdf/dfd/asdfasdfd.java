332106ec66f7f622f5b5fe862212196bc38fdc6b
user2 <user2@KH_C>
2017-06-16 16:29:31 +0900

    dfdfdf

--- dfdfdf/src/dfdfdf/dfd/asdfasdfd.java
+++ dfdfdf/src/dfdfdf/dfd/asdfasdfd.java
@@ -1,11 +1,10 @@
-package ssdd.ww;
+package dfdfdf.dfd;
 
-public class adads {
+public class asdfasdfd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//ASDFADSFASDFSDF
-		
+
 	}
 
 }
